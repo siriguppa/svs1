8b9b642a55c54ee0eda8ffece01b7e993c21469f
siriguppa venkat sai <64726256+siriguppa@users.noreply.github.com>
2022-03-17 12:25:20 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -4,7 +4,7 @@ from github import Github
 #access_token = input( "enter acess token")
 token = os.environ.get("GIT_TOKEN")
 
-org_name = os.environ.get("org")
+#org_name = os.environ.get("org")
 #g = Github(access_token)
 g = Github(token)
 
@@ -15,7 +15,7 @@ g = Github(token)
 #repo = g.get_organization("siriguppavenkatsai").get_repo("V") 
 
 
-for repo in g.get_organization(org_name).get_repos(): 
+for repo in g.get_organization("siriguppavenkatsai").get_repos(): 
 #for repo in g.get_user().get_repos(): 
  for branch in repo.get_branches():
         b = repo.get_branch(branch.name)
